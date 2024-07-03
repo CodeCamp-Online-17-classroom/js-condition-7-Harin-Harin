8d89fdc66dfa33b5689487f704f2a3279aea109d
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-03 17:00:24 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let a = Number(prompt("ใส่ค่าตัวเลขตัวที่ 1"));
+let b = Number(prompt("ใส่ค่าตัวเลขตัวที่ 2"));
+let sum = a + b;
+console.log(a);
+console.log(b);
+console.log(sum);
+if (isNaN(sum)){
+    alert('Invalid number');
+} else{
+    alert(`ผลรวมคือ ${sum}`);}
